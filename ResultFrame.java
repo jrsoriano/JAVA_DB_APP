964db3cdb1a5f66a829cb9928bf2b6949fd4e99d
Jose R. Soriano <jrsoriano@users.noreply.github.com>
2017-07-16 10:36:28 -0500

    Update ResultFrame.java

--- ResultFrame.java
+++ ResultFrame.java
@@ -1,13 +1,10 @@
 /**
- * Final Project 
  * This application will create,delete,update,and display employee records stored
  * in a db.
  * 
  * @author Jose R. Soriano
  * @version 1.0
  * 
- * Course: COMP-1348 Programming (Java 4)
- * Section: Online
  * Date Created: 12.31.2016
  * Last Update: 01.28.2017
 **/
